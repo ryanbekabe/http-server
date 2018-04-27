b5df58190cb56891d6c02e068b8f256d92120509
beremaran <beremaran@gmail.com>
2018-04-27 05:02:34 +0300

    forgotten line feed

--- main.c
+++ main.c
@@ -418,7 +418,7 @@ int main(int argc, char **argv) {
 
 
     if (argc < 2) {
-        fprintf(stderr, "usage: bserver port");
+        fprintf(stderr, "usage: bserver port\n");
         exit(1);
     }
 
